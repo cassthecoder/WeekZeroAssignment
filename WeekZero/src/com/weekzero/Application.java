619518369f29e9011cdb510085d8ca1a2d261f20
Cassandra Matos <cassandra@matosmendez.com>
2021-01-26 21:17:48 -0700

    added week zero assignment

---
+++ WeekZero/src/com/weekzero/Application.java
@@ -0,0 +1,12 @@
+package com.weekzero;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hey Class, welcome to Back End!");
+		
+	}
+
+}
